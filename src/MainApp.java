b113e539cb76f064ffc1dee773609de47c36edc1
lauratheresam <116265051+lauratheresam@users.noreply.github.com>
2022-12-10 13:40:45 +0700

    Add files via upload

---
+++ src/MainApp.java
@@ -0,0 +1,9 @@
+package src;
+
+public class MainApp {
+    public static void MainApp(String [] args) throws Exception {
+        System.out.println("Class Sampan");
+        Kapal kapal1 = new Kapal(50, 20000, "");
+        kapal1.informasi
+    }
+}
